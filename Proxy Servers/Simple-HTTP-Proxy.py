9f441467db894e9c7832c9cf132dfa39982d9113
themertzy <58080566+themertzy@users.noreply.github.com>
2020-05-18 08:34:25 -0500

    Update Simple-HTTP-Proxy.py

--- Proxy Servers/Simple-HTTP-Proxy.py
+++ Proxy Servers/Simple-HTTP-Proxy.py
@@ -13,4 +13,4 @@ print "serving at port", PORT
 httpd.serve_forever()
 
 #The server ignores drive letters and relative path names (such as ‘..’). However, it does not implement any other access control mechanisms, so be careful how you use it.
-#The second example implements a truly minimal web proxy. When sent to a proxy, the HTTP requests should include the full URI for the target server. This server uses urllib to fetch data from the target.
+#When sent to a proxy, the HTTP requests should include the full URI for the target server. This server uses urllib to fetch data from the target.
